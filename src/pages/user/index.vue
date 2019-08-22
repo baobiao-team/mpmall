c09f8fefee6c843b811af33e1b7e606b6a65f6b9
wangwc <820653253@qq.com>
2019-08-23 00:48:16 +0800

    修改了 user/index.vue文件

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    <van-button type="primary">主要按钮</van-button>
+    <van-button type="primary" @click="clickMe()">主要按钮</van-button>
   </div>
 </template>
 
 <script>
+  export default {
+    methods: {
+      clickMe() {
+        console.log("you click me")
+      }
+    }
+  }
 </script>
 
 <style>

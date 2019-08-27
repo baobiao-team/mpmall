b4361141c279f2571549bcfdd430c549da097451
yanhouhua <1393157362@qq.com>
2019-08-27 13:38:35 +0800

    修改了购物车页面

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,105 +1,71 @@
 <template>
   <div>
-    <van-checkbox-group class="card-goods" v-model="checkedGoods">
-    <van-checkbox
-    class="card-goods__item"
-    v-for="item in goods"
-    :key="item.id"
-    :name="item.id">
-    <van-card
-      :title="item.title"
-      :desc="item.desc"
-      :num="item.num"
-      :price="formatPrice(item.price)"
-      :thumb="item.thumb"
-    >
-      <view slot="footer">
-        <van-checkbox v-model="checked">复选框</van-checkbox>
-        <van-stepper v-model="value" min="1" max="5" />
-      </view>
-    </van-card>
-    </van-checkbox>
-    </van-checkbox-group>
-      <!-- 商品1展示 -->
+    <!-- 标题展示 -->
+    <div class="userinfo" @click="bindViewTap">
 
+        <card :text="userInfo.nickName"></card>
+    </div>
 
+    <div class="usermotto">
+      <van-checkbox-group class='card-goods' v-model='checkedGoods'>
+        <van-checkbox class='card-goods_item' v-for = "item in goods" :key="item.id"
+        :name="item.id" @click='clickMe' v-model="checkVal">
+          <van-card
 
+            :tag="item.tag"
+            :price = "item.price"
+            :title="item.title"
+            :desc="item.desc"
+            :thumb="item.thumb"
+          >
+            <view slot="footer">
 
+              <van-stepper v-model="item.value" min="1" max="5" />
 
-        <!-- 商品2展示 -->
-        <van-card
+            </view>
+          </van-card>
+        </van-checkbox>
 
-          tag="上衣"
-          price="10.00"
-          desc="描述信息"
-          title="商品标题"
-
-        >
-          <view slot="footer">
-            <van-checkbox v-model="checked">复选框</van-checkbox>
-            <van-stepper v-model="value" min="1" max="5" />
-          </view>
-        </van-card>
-        <!-- 商品3展示 -->
-        <van-card
-
-          tag="裤子"
-          price="10.00"
-          desc="描述信息"
-          title="商品标题"
-
-        >
-          <div slot="footer">
-              <van-checkbox v-model="checked">复选框</van-checkbox>
-              <van-stepper v-model="value" min="1" max="5" />
-          </div>
-        </van-card>
+      </van-checkbox-group>
+      </div>
 
       <div class="submit">
         <!-- 提交按钮 -->
         <van-submit-bar
-          :price="3050"
-          button-text="提交订单"
+          :price="totalPrice"
+          :disabled="!checkedGoods.length"
+          :button-text="submitBarText"
           @submit="onSubmit"
         >
-          <van-checkbox v-model="checked">全选</van-checkbox>
+          <van-checkbox v-model="checked" @click='clickAll'>全选</van-checkbox>
         </van-submit-bar>
       </div>
-</div>
 
+  </div>
 
 
 </template>
 
 <script>
-import { Checkbox, CheckboxGroup, Card, SubmitBar, Toast } from 'vant';
+import card from '@/components/card'
 
 export default {
-  components: {
-    [Card.name]:Card,
-    [Checkbox.name]:Checkbox,
-    [SubmitBar.name]:SubmitBar,
-    [CheckboxGroup.name]:CheckboxGroup,
-
-  },
   data () {
     return {
-      checkedGoods:['1','2','3',],
+      checkedGoods: ['1', '2', '3'],
+      motto: '您好！',
       goods:[{
         id: '1',
-        title: '进口香蕉',
-        desc: '约250g，2根',
-        price: 200,
-        num: 1,
-        thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/2f9a36046449dafb8608e99990b3c205.jpeg'
-      },
-      {
+        price:10,
+        title:'香蕉',
+        desc:'约250g,2根',
+        tag:'食品类',
+      },{
         id: '2',
-        title: '陕西蜜梨',
-        desc: '约600g',
-        price: 690,
-        num: 1,
-        thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/f6aabd6ac5521195e01e8e89ee9fc63f.jpeg'
+        price:300,
+        title:'运动鞋',
+        desc:'品牌：耐克',
+        tag:'服装类',
       },
        {
         id: '3',
@@ -108,29 +74,63 @@ export default {
         price: 2680,
         num: 1,
         thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/320454216bbe9e25c7651e1fa51b31fd.jpeg'
-      }]
-      };
-      },
-    computed: {
+      }],
+
+      card-goods_item.checkVal:return false;
+      checked: false,
+      userInfo: {
+        nickName: '购物车',
+      }
+    }
+  },
+  components: {
+    card,
+
+  },
+computed: {
     submitBarText() {
       const count = this.checkedGoods.length;
       return '结算' + (count ? `(${count})` : '');
+
     },
-     totalPrice() {
-          return this.goods.reduce((total, item) => total + (this.checkedGoods.indexOf(item.id) !== -1 ? item.price : 0), 0);
+    totalPrice() {
+      return this.goods.reduce((total, item) => total + (this.checkedGoods.indexOf(item.id) !== -1 ? item.price : 0), 0);
+      console.log(total);
+      console.log(item);
+      item.value()
+      console.log(this.checkedGoods.indexOf(item.id));
 
-        },
-        }
-
-
-  methods: {
-    formatPrice(price) {
-          return (price / 100).toFixed(2);
-        },
-        onSubmit() {
-          Toast('点击结算');
-        },
+    }
   },
+  methods: {
+
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    formatPrice(price){
+      return price.toFixed(2);
+    },
+
+    clickMe(){
+            console.log(this.checkVal);
+            item.checkVal =!item.checkVal;
+            },
+    clickAll(){
+      console.log(this.checkVal);
+      this.id.checkVal =!this.id.checkVal;
+      console.log(this.checked);
+      this.checked=!this.checked;
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
 }
 </script>
 

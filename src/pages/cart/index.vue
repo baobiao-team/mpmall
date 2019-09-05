5fb8c297fe6dee917e59cad3919b958513d4915a
yanhouhua <1393157362@qq.com>
2019-09-05 09:54:23 +0800

    change cart

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    <div class="goodss">
-    <van-checkbox-group class="card-goods" v-model="checkedGoods" :value="result" data-key="result" @change="onChange">
+
+   <!-- <div >
+     <input type="checkbox" id="jack" value="Jack" v-on:input="$emit('input', $event.target.value)">
+     <label for="jack">Jack</label>
+
+     <br>
+     <span>Checked names: {{ value }}</span>
+     cos
+   </div> -->
+
+    <div class="goodss" id="checkbox">
+
+    <van-checkbox-group class="card-goods" v-model="result" :value="result" data-key="result" @change="onChange">
       <van-checkbox
         class="card-goods__item"
         v-for="item in goods"
@@ -16,7 +27,16 @@
           :price='item.price'
           :thumb="item.thumb"
           :num="item.num"
-        />
+        >
+        <div slot="tags">
+            <van-tag plain type="danger">标签</van-tag>
+            <van-tag plain type="danger">标签</van-tag>
+          </div>
+          <div slot="footer">
+            <van-button size="mini" @click.stop="cutGoods(event)">-1</van-button>
+            <van-button size="mini" @click.stop="addGoods()">+1</van-button>
+          </div>
+          </van-card>
    <!-- <van-stepper  min="1" max="5" :key="item.id"
     :name="item.id"/> -->
     <!-- <van-stepper
@@ -35,24 +55,23 @@
       :button-text="submitBarText"
       @submit="onSubmit"
     >
-
-
    <!-- <van-cell title="已选" :value="" :label="cellText"> -->
   <!--  </van-cell> -->
     </van-submit-bar>
     </div>
   </div>
 </template>
-
+<script src="https://cdn.jsdelivr.net/npm/vue"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
 <script>
 import card from '@/components/card'
 import Toast from '../../../static/vant/toast/toast'
 export default {
-
   data() {
     return {
-      checkedGoods: ['1', '2', '3'],
-
+      num:"1",
+      // checked:"true",
+      // checkedGoods: [],
       goods: [{
         id: '1',
         title: '进口香蕉',
@@ -87,11 +106,10 @@ export default {
       const count = this.result.length;
       return '结算' + (count ? `(${count})` : '');
     },
-    cellText(){
-      const count = this.result.length;
-      return  (count ? `(${count})` : '')+'件' ;
-    },
-
+    // cellText(){
+    //   const count = this.result.length;
+    //   return  (count ? `(${count})` : '')+'件' ;
+    // },
     totalPrice() {
       console.log(this.goods.reduce((total, item) => total + (this.result.indexOf(item.id) !== -1 ?  Number(item.price) : 0), 0))
       return this.goods.reduce((total, item) => total + (this.result.indexOf(item.id) !== -1 ? Number(item.price) : 0), 0);
@@ -99,6 +117,18 @@ export default {
 
   },
   methods: {
+     checkboxChange: function (e) {
+                    var items = this.items,
+                        values = e.detail.value;
+                    for (var i = 0, lenI = items.length; i < lenI; ++i) {
+                        const item = items[i]
+                        if(values.includes(item.value)){
+                            this.$set(item,'checked',true)
+                        }else{
+                            this.$set(item,'checked',false)
+                        }
+                    }
+                },
      formatPrice(price) {
        // let datas = [];
        // return this.goods.filter(good => {
@@ -112,7 +142,9 @@ export default {
       const {key} = event.mp.currentTarget.dataset;
       //this.setData({[key]: event.mp.detail});
       this[key]=event.mp.detail;
+      console.log("-----------------------")
       console.log('result: ',this.result.sort())
+      console.log("-----------------------")
     },
     toggle(event) {
       const {name} = event.mp.currentTarget.dataset;
@@ -121,6 +153,16 @@ export default {
       console.log('box: ',box);
       box.toggle();
     },
+    cutGoods(){
+      console.log("商品减少一个");
+      this.num--;
+      console.log(this.num);
+    },
+    addGoods()
+    {
+      console.log("商品增加一个");
+      this.num++;
+    },
     onSubmit() {
       Toast('点击结算');
     },
@@ -128,10 +170,29 @@ export default {
 
         },
 
-      },
-      // 获取传递过来的商品参数
+    async request(){
+              wx.request({
+                url: 'http://localhost:8081/user/cartinfo?UserId=0011', //仅为示例，并非真实的接口地址
+                data: {
+                   UserId: '0011',
+                },
+                header: {
+                  'content-type': 'application/json' // 默认值
+                },
+                success (res) {
+                  console.log("请求成功！")
+                  console.log(res.data)
+                }
+              })
+            }
+
+
+  },
+      // 获取从商品详情传递过来的商品参数
       onLoad() {
+        this.request();
         try {
+
           var value = wx.getStorageSync('goods')
           if (value) {
             // Do something with return value
@@ -142,11 +203,11 @@ export default {
             tmpGood.price = value.goodsPrice
             tmpGood.thumb = value.imageUrl
             this.goods.push(tmpGood)
-
           }
         } catch (e) {
           // Do something when catch error
         }
+
 
             // let arr = JSON.parse(option.goods)
             // // console.log(option.text)
